2b0c815a812c63d4198f76fd59433ba8326dc2f4
Aphelele Joyi <aphelelejoyi@gmail.com>
2023-08-19 22:34:13 +0200

    products

--- dazzlingjewels_gui/src/views/ProductsView.vue
+++ dazzlingjewels_gui/src/views/ProductsView.vue
@@ -1,45 +1,48 @@
+
 <template>
-    <div class="container">
-      <div class="row">
-        <h2 class="display-2">Products</h2>
-      </div>
-      <div class="row justify-content-center gap-3" v-if="Products">
-        <div v-for="Product in Products" class="col-md-4 mb-3" :key="Product.prodID">
-          <div class="card">
-            <img :src="Product.image" class="card-img-top" :alt="Product.prodName" />
-            <div class="card-body">
-              <h5 class="card-title">{{ Product.prodName }}</h5>
-              <p class="card-text">Category: {{ Product.category }}</p>
-              <h5 class="card-title"> R{{ Product.amount }}</h5>
-            </div>
+  <div class="container">
+    <div class="row">
+      <h2 class="display-2">Products</h2>
+    </div>
+    <div class="row justify-content-center gap-3" v-if="Products">
+      <div v-for="Product in Products" class="col-2" :key="Product.prodID">
+        <div class="col">
+          <img :src="Product.image" class="card-img-top img fluid" :alt="Product.prodName" style="height:15rem" />
+          <div class="card-body">
+            <h5 class="card-title">{{ Product.prodName }}</h5>
+            <p class="card-text">Category: {{ Product.category }}</p>
+            <h5 class="card-title"> R{{ Product.amount }}</h5>
           </div>
         </div>
       </div>
-      <div v-else class="row justify-content-center">
-        <SpinnerCompVue />
-      </div>
     </div>
-  </template>
-  
-  
+    <div v-else class="row justify-content-center">
+      <SpinnerCompVue />
+    </div>
+  </div>
+</template>
+
+
 
 <script>
 import SpinnerCompVue from '../components/SpinnerComp.vue';
 
 export default {
-  components: {
-    SpinnerCompVue
-  },
-  computed: {
-    Products() {
-      return this.$store.state.Products;
-    }
-  },
-  mounted() {
-    this.$store.dispatch('fetchProducts');
+components: {
+  SpinnerCompVue
+},
+computed: {
+  Products() {
+    return this.$store.state.Products;
   }
+},
+mounted() {
+  this.$store.dispatch('fetchProducts');
+}
 };
 </script>
+
+styles
 
 <style scoped>
 
