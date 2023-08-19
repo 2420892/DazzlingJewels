dec18f4ac90a25bc61fddbe34dbff9d49ff8d5ce
siyamanga <zwenisiyamanga@gmail.com>
2023-08-19 22:23:02 +0200

    start

--- dazzlingjewels_gui/src/views/HomeView.vue
+++ dazzlingjewels_gui/src/views/HomeView.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="home">
-    <h2>You are home</h2>
+  <div>
+    <nav class="navbar" style="background-color: white!important;">
+      <div class="container-fluid">
+        <a class="navbar-brand">DazzlingJewels</a>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    
+    <!-- Horizontal Line -->
+    <hr class="horizontal-line">
+   
+    <nav class="navbar" style="background-color: white!important;">
+      <form class="container-fluid justify-content-start">
+        <a class="btn me-5" type="button" href="./ProductsView.vue">ShopNow</a>
+        <a class="btn btn-sm me-5" type="button" href="">Watches</a>
+        <a class="btn btn-sm me-5" type="button" href="">Rings</a>
+        <a class="btn btn-sm me-5" type="button" href="">Necklaces</a>
+      </form>
+    </nav>
   </div>
 </template>
 
@@ -9,3 +29,7 @@ export default {
   
 }
 </script>
+
+<style>
+
+</style>
