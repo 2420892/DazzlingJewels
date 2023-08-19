d8f88f2bbbc9dfb6aa992301009f27e38726c457
Aphelele2 <aphelelejoyi@gmail.com>
2023-08-19 14:09:52 +0200

    products

---
+++ dazzlingjewels_gui/src/store/index.js
@@ -0,0 +1,59 @@
+import { createStore } from 'vuex';
+import axios from 'axios';
+
+const cUrl = "https://dazzlingjewels.onrender.com/";
+
+export default createStore({
+  state: {
+    users: null,
+    user: null,
+    Products: null,
+    Product: null,
+    spinner: false,
+    token: null,
+    msg: null
+  },
+  getters: {},
+  mutations: {
+    setUsers(state, users) {
+      state.users = users;
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
+    setProducts(state, Products) {
+      state.Products = Products;
+    },
+    setProduct(state, Product) {
+      state.Product = Product;
+    },
+    setSpinner(state, value) {
+      state.spinner = value;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+    setMsg(state, msg) {
+      state.msg = msg;
+    }
+  },
+  actions: {
+    async fetchUsers(context) {
+      try {
+        const { data } = await axios.get(`${cUrl}users`);
+        context.commit("setUsers", data.results);
+      } catch (e) {
+        context.commit("setMsg", "An error occurred");
+      }
+    },
+    async fetchProducts(context) {
+      try {
+        const { data } = await axios.get(`${cUrl}Products`);
+        context.commit("setProducts", data.results);
+      } catch (e) {
+        context.commit("setMsg", "An error occurred");
+      }
+    }
+  },
+  modules: {}
+});
---
+++ dazzlingjewels_gui/src/views/ProductsView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+      <div class="row">
+        <h2 class="display-2">Products</h2>
+      </div>
+      <div class="row justify-content-center gap-3" v-if="Products">
+        <div v-for="Product in Products" class="col-md-4 mb-3" :key="Product.prodID">
+          <div class="card">
+            <img :src="Product.image" class="card-img-top" :alt="Product.prodName" />
+            <div class="card-body">
+              <h5 class="card-title">{{ Product.prodName }}</h5>
+              <p class="card-text">Category: {{ Product.category }}</p>
+              <h5 class="card-title">{{ Product.amount }}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else class="row justify-content-center">
+        <SpinnerCompVue />
+      </div>
+    </div>
+  </template>
+  
+  
+
+<script>
+import SpinnerCompVue from '../components/SpinnerComp.vue';
+
+export default {
+  components: {
+    SpinnerCompVue
+  },
+  computed: {
+    Products() {
+      return this.$store.state.Products;
+    }
+  },
+  mounted() {
+    this.$store.dispatch('fetchProducts');
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
